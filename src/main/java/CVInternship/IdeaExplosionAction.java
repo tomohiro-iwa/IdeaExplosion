25f63e59e19f783ec744e5290e17886d753cb927
Titan111 <tomo_develop_singularity_111@ezweb.ne.jp>
2018-04-02 17:57:28 +0900

    ページサイズの制限を解除

--- src/main/java/CVInternship/IdeaExplosionAction.java
+++ src/main/java/CVInternship/IdeaExplosionAction.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,6 +46,11 @@ public class IdeaExplosionAction implements IPluginActionDelegate {
 			String message = "Project is not opened.Please open the project or create new project.";
 			JOptionPane.showMessageDialog(window.getParent(), message, "Warning", JOptionPane.WARNING_MESSAGE);
 		} catch (Exception e) {
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			pw.flush();
+			System.out.println(sw.toString());
 			JOptionPane.showMessageDialog(window.getParent(), "Unexpected error has occurred.", "Alert", JOptionPane.ERROR_MESSAGE);
 			throw new UnExpectedException();
 		}
@@ -55,8 +62,9 @@ public class IdeaExplosionAction implements IPluginActionDelegate {
 		List<String> relation_words = new ArrayList<String>();
 
 		String wiki_ja = "https://ja.wikipedia.org/wiki/";
-		Document word_html = Jsoup.connect(wiki_ja+serch).get();
+		Document word_html = Jsoup.connect(wiki_ja+serch).maxBodySize(0).get();
 		Elements cat_tags = word_html.select("#mw-normal-catlinks").select("a");
+		System.out.println(word_html.outerHtml());
 		Element last_cat_tag = cat_tags.last();
 
 		Document cat_html = Jsoup.connect(wiki_ja + "Category:"+last_cat_tag.text()).get();
@@ -84,6 +92,4 @@ public class IdeaExplosionAction implements IPluginActionDelegate {
 		}
 		TransactionManager.endTransaction();
 	}
-
-
 }
